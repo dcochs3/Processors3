64586b98b74642d68c3e596d7014bdbba5f8dd77
Dana Ochs <dcochs3@gmail.com>
2019-03-09 14:52:33 -0500

    changed file to use test.mif and test.hex

--- Project.v
+++ Project.v
@@ -25,8 +25,8 @@ module Project(
     parameter ADDRSW    = 32'hFFFFF090;
 
     // Change this to fmedian2.mif before submitting
-//    parameter IMEMINITFILE = "Test.mif";
-    parameter IMEMINITFILE = "fmedian2.mif";
+    parameter IMEMINITFILE = "Test.mif";
+    //parameter IMEMINITFILE = "fmedian2.mif";
 
     parameter IMEMADDRBITS = 16;
     parameter IMEMWORDBITS = 2;
@@ -131,8 +131,8 @@ module Project(
     // This statement is used to initialize the I-MEM
     // during simulation using Model-Sim
     initial begin
-        $readmemh("fmedian2.hex", imem);
-        $readmemh("fmedian2.hex", dmem);
+        $readmemh("test.hex", imem);
+        //$readmemh("fmedian2.hex", dmem);
     end
 
     assign inst_FE_w = imem[PC_REG[IMEMADDRBITS-1:IMEMWORDBITS]];
